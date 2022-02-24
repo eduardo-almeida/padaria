aa38f2f601314f1e06eee071e65cdb37e4f5fd63
Eduardo Almeida <eduardoeandroid@gmail.com>
2022-02-24 15:02:32 -0300

    Novos arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'pad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pad-app',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'padaria';
+export class AppComponent  implements OnInit {
+
+  content = 'Welcome do Meat App!!!!'
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
